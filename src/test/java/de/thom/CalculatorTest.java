5743ebe1c9344357a3f46d6ce90b87a75f54d312
thom.ralf <thom.ralf@pearl.de>
2019-08-26 19:57:59 +0200

    ports part extended

---
+++ src/test/java/de/thom/CalculatorTest.java
@@ -0,0 +1,15 @@
+package de.thom;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class CalculatorTest {
+     private Calculator calculator = new Calculator();
+
+     
+     @Test
+     public void testSum() {
+          assertEquals(5, calculator.sum(2, 3));
+     }
+}
