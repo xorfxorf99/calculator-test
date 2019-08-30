61dcbd3d3935513216987679e84a5c563230744b
thom.ralf <thom.ralf@pearl.de>
2019-08-30 15:09:43 +0200

    calculator url hardcoded

--- src/main/java/de/thom/CalculatorController.java
+++ src/main/java/de/thom/CalculatorController.java
@@ -1,7 +1,9 @@
 package de.thom;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,15 @@ class CalculatorController {
 	@Autowired
      private Calculator calculator;
 
-     @RequestMapping("/sum")
+     @RequestMapping(method=RequestMethod.GET, value="/sum")
      String sum(@RequestParam("a") Integer a, 
                 @RequestParam("b") Integer b) {
           return String.valueOf(calculator.sum(a, b));
      }
+     
+     @RequestMapping(method=RequestMethod.GET, value="/healthz")
+ 	 public String healthCheck()
+ 	 {
+ 		return "OK";
+ 	 }
 }
