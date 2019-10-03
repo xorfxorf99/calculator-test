c7b8cd2dabebdada25031b1bd8dfb6ccd1cdf5d4
thom.ralf <thom.ralf@pearl.de>
2019-10-03 11:57:35 +0200

    Cucumber pretty

--- src/test/java/acceptance/AcceptanceTest.java
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -6,5 +6,6 @@ import org.junit.runner.RunWith;
 
 /** Acceptance Test */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:acceptance")
+@CucumberOptions(plugin = { "pretty", "html:target/cucumber",
+"junit:target/junit-report.xml" }, features ="classpath:acceptance")
 public class AcceptanceTest { }
